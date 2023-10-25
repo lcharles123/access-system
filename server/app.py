c95495a5af0024b69d592c738b753db81f1a45d2
lcharles123 <root@racknerd-86b201>
2023-10-24 22:42:12 -0400

    Migrated all code from app.py to main.py

--- server/app.py
+++ server/app.py
@@ -23,95 +23,6 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 
-''' A page used to add users
-    It can be used a enrollment_id to add user to room_number
-    After the request for adding, a message is displayed
-    indicating success or error with its 'error type'
-    'error type' can be one of {WrongUser, WrongRoom, AlreadyAdded, Unknown}
-'''
-@app.route('/users', methods=('GET', 'POST'))
-def users():
-    if request.method == 'POST':
-        user_id = None
-        room_id = None
-        try:
-            user_id = request.form['user_id']
-            room_id = request.form['room_id']
-        except:
-            flash('Corrupted data sent!', 'alert_fail')
-        
-        if not room_id or not user_id:
-            if not room_id:
-                flash('Room ID is required!', 'alert_fail')
-            if not user_id:
-                flash('User ID is required!', 'alert_fail')
-        else:
-            flash('User and room added', 'alert_ok')
-        
-
-        if True:
-            pass
-        else:
-            pass
-            '''conn = get_db_connection()
-            conn.execute('INSERT INTO main (room_id, user_id, succeded) VALUES (?, ?, ?)',
-                         (title, content, 'TRUE'))
-            conn.commit()
-            conn.close()
-            return redirect(url_for('index'))'''
-            flash('User added!', 'alert_ok')
-    permissions = get_permissions_list()
-    
-    
-    return render_template('users.html', permissions=permissions)
-
-#FIXME dummy function for access local db
-''' Get room list access from local DB 
-    @return a row object, such as r['col1'],r['col2']... for r int row 
-'''
-def get_permissions_list():
-    db_path = os.path.join(BASE_DIR, "database/permissions.db")
-    conn = sqlite3.connect(db_path)
-    conn.row_factory = sqlite3.Row
-    q_res = conn.execute('SELECT * FROM permissions').fetchall()
-    conn.close()
-    return q_res
-
-''' Get list of rooms included in LDAP server and exhibit them in a table
-    Can include new rooms in LDAP server
-'''
-@app.route('/rooms', methods=('GET', 'POST'))
-def rooms():
-    if request.method == 'POST':
-        room_id = None
-        # room_name is got from ldap?
-        try:
-            room_id = request.form['room_id']
-        except:
-            flash('Corrupted data sent!', 'alert_fail')
-       
-        if not room_id:
-            flash('Room ID is required!', 'alert_fail')
-        else:
-            flash('Room ID added!', 'alert_ok')
-            '''conn = get_db_connection()
-            conn.execute('INSERT INTO main (room_id, user_id, succeded) VALUES (?, ?, ?)',
-                         (title, content, 'TRUE'))
-            conn.commit()
-            conn.close()
-            return redirect(url_for('index'))'''
-    
-    rooms = get_rooms()
-    
-    return render_template('rooms.html', rooms=rooms)
-
-def get_rooms():
-    db_path = os.path.join(BASE_DIR, "database/rooms.db")
-    conn = sqlite3.connect(db_path)
-    conn.row_factory = sqlite3.Row
-    q_res = conn.execute('SELECT * FROM rooms').fetchall()
-    conn.close()
-    return q_res
 
 
 ''' Do logout from server
@@ -128,23 +39,6 @@ def login():
         return redirect(url_for('index'))
     
     return render_template('login.html')
-
-'''
-'''
-#TODO minimal api here https://flask-restful.readthedocs.io/en/latest/quickstart.html#a-minimal-api
-# once a class is defined, from Resource, you can add methods that are mapped to http methods
-# using api.add_resource, its possible to add a class to it and any desired endpoint ie. api.add_resource(Todo, '/todos/<todo_id>')
-
-class Lock_Api(Resource):
-    def get(self):
-        return {'hello': 'get'}
-    def post(self):
-        # TODO check if data is valid
-        # consult db of permissions
-        # respond with granted or denied
-        return {'hello': 'post'}
-
-api.add_resource(Lock_Api, '/api')
 
 
 @app.route('/<int:post_id>')
